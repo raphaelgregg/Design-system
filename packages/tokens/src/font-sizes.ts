7032a4a52fc15bd73e83e39611f6e3102eeb553b
Raphael Gregg <raphaelgregg@gmail.com>
2024-03-08 17:48:00 -0300

    first commit

---
+++ packages/tokens/src/font-sizes.ts
@@ -0,0 +1,33 @@
+// root {font-size: 62.5%;}
+export const fontSizes = {
+  xxs: '1rem', // '10px',
+  xs: '1.2rem', // '12px',
+  sm: '1.4rem', // '14px',
+  md: '1.6rem', // '16px',
+  lg: '1.8rem', // '18px',
+  xl: '2rem', // '20px',
+  '2xl': '2.4rem', // '24px',
+  '4xl': '3.2rem', // '32px',
+  '5xl': '3.6rem', // '36px',
+  '6xl': '4.8rem', // '48px',
+  '7xl': '6.4rem', // '64px',
+  '8xl': '7.2rem', // '72px',
+  '9xl': '9.6rem', // '96px',
+}
+
+// root padrão
+export const fontSizes2 = {
+  xxs: '0.625rem', // '10px',
+  xs: '0.75rem', // '12px',
+  sm: '0.875rem', // '14px',
+  md: '1rem', // '16px',
+  lg: '1.125rem', // '18px',
+  xl: '1.25rem', // '20px',
+  '2xl': '1.5rem', // '24px',
+  '4xl': '2rem', // '32px',
+  '5xl': '2.25rem', // '36px',
+  '6xl': '3rem', // '48px',
+  '7xl': '4rem', // '64px',
+  '8xl': '4.5rem', // '72px',
+  '9xl': '6rem', // '96px',
+}
---
+++ packages/tokens/src/fonts.ts
@@ -0,0 +1,5 @@
+export const fonts = {
+  default: 'Roboto, sans-serif',
+  heading: 'Poppins, sans-serif',
+  code: 'monospace',
+}
